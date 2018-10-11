65b8e9679b48e97fd2eb70d89e46f291348f3287
Dmecam <mao97mangaka@gmail.com>
2018-10-11 06:52:01 +0200

    Changing sketch.js

--- Libro-de-artista/sketch.js
+++ Libro-de-artista/sketch.js
@@ -1,110 +1,138 @@
-//define an empty array
-let circle = [];
-//define a new variable
-let clicks = 2;
+//define the variable clicks
+var clicks = 4;
 
+//setup
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(125);
-  //set the frame rate
-  frameRate(15);
-
-  //push in three new values
-  circle.push(0, 0, 60);
+	createCanvas(windowWidth, windowHeight);
 }
 
-//define variable
-var r = circle[2] * 0.5;
+//define variables
+let pos_x = 0;
+let pos_y = 0;
+let d = 60;
+let r = d/2;
+
 
 function draw() {
-  //draw a group of circles using the array elements
-  for( var i = 0; i < 15; i++ ) {
-      circle[0] = width * i/10 + r;
+  //set the background
+	background(125);
+  //define variable quanty
+	var quanty = map(mouseY, 0, height, 20, 70);
 
-      //if the number of clicks is greater than 3, the circles do not have strokes
-      if (clicks < 3) {
+//if 1 click
+	if (clicks == 1) {
+    //change the background
+    background(200);
+
+    //text
+    noStroke();
+    fill ('rgb(174, 223, 233)');
+    rect( 10, 39, 145, 30, 50);
+
+    fill ( 0 );
+    textSize ( 20 );
+    text( "Click to change", 15, 60);
+
+		// interactive circle line
+		for( var i=0; i < 15; i++ ) {
+		    pos_x = width * i/10 + r;
         noStroke();
-        fill(random(256), mouseY, random(256), mouseX);
-      }
+        fill(pos_x, mouseY, pos_y, mouseX);
+        ellipse( pos_x, mouseY, d );
+		}
+	}
 
-      //if 3 is greater than the number of clicks, the circles do not have fill
-      else {
-        noFill();
-        stroke(random(256), mouseY, random(256), mouseX);
-        strokeWeight(random(6));
-      }
-      //draws a ellipse using the array elements
-      ellipse( random(width), random(height), circle[2] );
-    }
-  //run the inst function
-  inst();
-  //define two new variables
-  let num1 = random(height);
-  let num2 = random(height);
-  //complete the array
-  randomLine(num1, num2);
-  //run the newCursor function
-  newCursor();
-}
+  //if 2 clicks
+	else if (clicks == 2) {
+    //change the background
+    background(125);
 
-function backgroundChange() {
-  //use the number of clicks to determinate the background
-  if (clicks == 1) {
-    background (5);
-  }
-  else if (clicks == 0) {
-    background (125);
-    //draw a group of lines
-    for( var j = 0; j < 30; j++ ) {
-      strokeWeight(random(10));
-      line (random(width), 0, random(width), height, 20);
+    //text
+    noStroke();
+    fill ('rgb(174, 223, 233)');
+    rect( 10, 39, 145, 30, 50);
+
+    fill ( 0 );
+    textSize ( 20 );
+    text( "Click to change", 15, 60);
+
+		// circle lines
+    fill(pos_x, mouseY, pos_y, mouseX);
+		let positionS = 0;
+    for (var x = 20; x <= width-20; x += quanty) {
+      ellipse (x, positionS, 20, 20);
+      positionS = positionS + 30;
+      if (positionS >= height) {
+        positionS = 0;
+      }
     }
-  }
-  else if (clicks == 2) {
-    background (random(256), random(256), random(256), random(200));
-  }
+	}
+
+  //if 3 clicks
   else if (clicks == 3) {
-    background (mouseX, mouseY, mouseX);
-  }
-  //run the inst function
-  inst();
+    //change the background
+    background(5);
+
+    //text
+    noStroke();
+    fill ('rgb(174, 223, 233)');
+    rect( 10, 39, 145, 30, 50);
+
+    fill ( 0 );
+    textSize ( 20 );
+    text( "Click to change", 15, 60);
+
+		// Circles
+		for (var x = 20; x <= width-20; x += quanty) {
+			for (var y = 20; y <= height-20; y+= quanty) {
+        noStroke();
+        fill(random(256), random(256), 127, 175);
+				ellipse(x, y, 30, 30);
+			}
+		}
+	}
+
+  //if 4 clicks
+  else if (clicks == 4) {
+    //change the background
+    background(pos_y, mouseY, mouseX);
+
+    //text
+    noStroke();
+    fill ('rgb(174, 223, 233)');
+    rect( 10, 39, 145, 30, 50);
+
+    fill ( 0 );
+    textSize ( 20 );
+    text( "Click to change", 15, 60);
+
+		// line draw
+		for (var x = 50; x < width-50; x += quanty) {
+			for (var y = 50; y < height-50; y+= quanty) {
+				for (var i = 0; i < 16; i+=4) {
+          stroke(20);
+          strokeWeight(1);
+					line(mouseX, x, mouseY, y );
+				}
+        noFill();
+        stroke(mouseY);
+				rect(x, y, 30, 30);
+			}
+		}
+	}
+
+  //update the pos_y value
+  pos_y++;
+    if (pos_y > height) {
+        pos_y = 0;
+    }
+
 }
 
 function mousePressed() {
-
-  //run the backgroundChange function
-  backgroundChange();
   //update the clicks value
 	clicks++;
 
-  //reset the clicks value if it is equal to 4
-	if (clicks == 4) clicks = 0;
-}
-
-//text
-function inst() {
-  noStroke();
-  fill ('rgb(174, 223, 233)');
-  rect( 10, 39, 145, 30, 50);
-
-  fill ( 0 );
-  textSize ( 20 );
-  text( "Click to change", 15, 60);
-
-}
-
-//draw a line
-function randomLine (posY, posY2) {
-  strokeWeight(random(6));
-  stroke(mouseY, mouseX, mouseY);
-  line(0, posY, width, posY2);
-}
-
-//new cursor
-function newCursor() {
-  noCursor();
-  stroke(mouseY, mouseY, mouseX);
-  strokeWeight(5);
-  fill( mouseX, mouseX, mouseY);
-  rect( mouseX + 15, mouseY + 15, 30, 30);
+  //reset the clicks value if it greater than 4
+	if (clicks > 4) clicks = 1;
 }
